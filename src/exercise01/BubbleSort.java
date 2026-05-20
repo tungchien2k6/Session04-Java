e47d5b02eab6599dedfc4b4b91cdf27ceb028aa3
TungThanh <tung111104@st.vimaru.edu.vn>
2026-05-20 23:33:22 +0700

    exercise01

---
+++ src/exercise01/BubbleSort.java
@@ -0,0 +1,34 @@
+package exercise01;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phần tử thứ " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nMảng sau khi sắp xếp giảm dần:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
